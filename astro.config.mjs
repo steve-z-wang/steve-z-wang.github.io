04d20a2e8c6f29ab7f1341902aec728dcf2709b1
Steve Wang <77543638+steve-z-wang@users.noreply.github.com>
2025-12-03 16:36:35 -0800

    Update base path to /blog/

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://steve-z-wang.github.io',
-  base: '/blog3',
+  base: '/blog/',
   integrations: [mdx()],
   vite: {
     plugins: [tailwindcss()],
